6df6cbd21a0650c6ce3c1628bf5235c02f242717
shiwei <401181980@qq.com>
2022-10-27 11:53:04 +0800

    config Knife4jConfig and create controller component

---
+++ service-system/src/main/java/com/stone/system/controller/SysRoleController.java
@@ -0,0 +1,33 @@
+package com.stone.system.controller;
+
+import com.stone.model.system.SysRole;
+import com.stone.system.service.SysRoleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api(tags = "角色管理接口")
+@RestController
+@RequestMapping("/admin/system/sysRole")
+//http://localhost:8800/admin/system/sysRole
+public class SysRoleController {
+    @Autowired
+    private SysRoleService sysRoleService;
+
+    @ApiOperation("查询所有记录")
+    @GetMapping("findAll")
+    public List<SysRole> findAllRole(){
+        List<SysRole> list = sysRoleService.list();
+        return list;
+    }
+
+    @ApiOperation("逻辑删除接口")
+    @DeleteMapping("remove/{id}")
+    public boolean removeRoleById(@PathVariable Long id){
+        boolean isSuccess = sysRoleService.removeById(id);
+        return isSuccess;
+    }
+}
