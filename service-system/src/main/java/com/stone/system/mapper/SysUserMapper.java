aae1f30d26647d850196d8ecf20fad3e44845807
shiwei <401181980@qq.com>
2022-10-31 12:56:28 +0800

    create sysUser relevant file

---
+++ service-system/src/main/java/com/stone/system/mapper/SysUserMapper.java
@@ -0,0 +1,17 @@
+package com.stone.system.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.stone.model.system.SysUser;
+
+/**
+ * <p>
+ * 用户表 Mapper 接口
+ * </p>
+ *
+ * @author stone
+ * @since 2022-10-31
+ */
+public interface SysUserMapper extends BaseMapper<SysUser> {
+
+}
---
+++ service-system/src/main/java/com/stone/system/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.stone.system.service.impl;
+
+import com.stone.model.system.SysUser;
+import com.stone.system.mapper.SysUserMapper;
+import com.stone.system.service.SysUserService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * <p>
+ * 用户表 服务实现类
+ * </p>
+ *
+ * @author stone
+ * @since 2022-10-31
+ */
+@Service
+public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> implements SysUserService {
+
+}
